fc06d4cd124051632a30027da023c0ef5c632668
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2022-07-08 11:00:38 +0100

    Update assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,9 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.reduce((acc,curr) => acc +curr,0);}
+   }
 
-assignment.sumOfNumbers = sumOfNumbers;
+//assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
